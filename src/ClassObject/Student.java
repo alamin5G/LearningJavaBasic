dcfeda6d3faf21cf68eb4c0781ec60fc465d8f1c
Md. Alamin <alaminrakib4864@gmail.com>
2022-11-17 18:33:44 +0600

    changing the access modifier for invoke from StudentMain.java class

--- src/ClassObject/Student.java
+++ src/ClassObject/Student.java
@@ -1,11 +1,11 @@
 package ClassObject;
 
 public class Student {
-    private int sId;
-    private double cgpa;
-    private String name;
-    private String phone;
-    private String dept;
+    protected int sId;
+    protected double cgpa;
+    protected String name;
+    protected String phone;
+    protected String dept;
 
     void display(){
         System.out.println(name);
@@ -16,25 +16,5 @@ public class Student {
         System.out.println("----------------------------------");
     }
 
-    public static void main(String[] args){
 
-        Student student1 = new Student();
-        System.out.println("Student Information: ");
-        student1.name   = "Md. Alamin";
-        student1.sId    = 21303134;
-        student1.dept   = "CSE";
-        student1.cgpa   = 3.71;
-        student1.phone  = "+8801822679672";
-
-        Student student2 = new Student();
-        student2.name   = "Md. Rakib";
-        student2.sId    = 21303134;
-        student2.dept   = "CSE";
-        student2.cgpa   = 3.83;
-        student2.phone  = "+8801822679672";
-
-
-        student1.display();
-        student2.display();
-    }
 }
