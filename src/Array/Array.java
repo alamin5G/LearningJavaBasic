17497045a5a9de6fb6ab08513319fb3d11cb269a
Md. Alamin <alaminrakib4864@gmail.com>
2022-12-21 20:07:46 +0600

    Array and its built-in function.

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -1,5 +1,6 @@
 package Array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array {
@@ -7,7 +8,8 @@ public class Array {
         Scanner input = new Scanner(System.in);
 
         int a[] = new int[5];
-        System.out.println("Insert Number: ");
+        int b[] = {2, 5, -2, -1, 4, 6, 8, 15, 1, 10};
+        /*System.out.println("Insert Number: ");
         for (int i = 0; i < 5; i++) {
             a[i] = input.nextInt();
 
@@ -17,6 +19,34 @@ public class Array {
 
         for (int i = 0; i < 5; i++) {
             System.out.print(a[i] + " ");
+        }*/
+
+        //Sorting array
+       /* Arrays.sort(b);
+        System.out.println("After Sorting: ");
+        for (int j : b) {
+            System.out.print(j + " ");
+        }*/
+
+
+        //Binary Search
+       /* System.out.println("\nBinary Search: input your item");
+        int item = input.nextInt();
+        int index = Arrays.binarySearch(b, item);
+        if (index >= 0) {
+            System.out.println("Found @ position: "+(index+1));
+        }else{
+            System.out.println("Not Found!");
+        }*/
+
+        //Linear Search
+        int target = 1;
+        for (int i = 0; i < b.length; i++) {
+            if(target==b[i]){
+                System.out.println("Find at postion : "+ (i+1));
+                break;
+            }
         }
+
     }
 }
